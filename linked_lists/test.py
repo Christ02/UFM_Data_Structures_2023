7f8b4cf9bcb8c77e78395c059d26d7df03222292
Christian <christianbarrios@ufm.edu>
2023-03-01 21:16:21 -0600

    Modify the above code so that instead of changing the class, create a new DoublyLinkedList class

--- linked_lists/test.py
+++ linked_lists/test.py
@@ -51,4 +51,35 @@ print(ll)
 ll.delete('C')
 print(ll)
 
+print('Activitie 1-------------------------------------------------------------------')
+
 print(ll.search('G'))  # output: True
+
+print('Activitie 2-------------------------------------------------------------------')
+
+
+from linked_list import DoublyLinkedList
+
+# Create a new playlist
+playlist = DoublyLinkedList()
+
+# Add some songs to the playlist
+playlist.insert_beginning('1', 'Dakiti', 'Bad Bunny ft. Jhay Cortez', 'El Último Tour Del Mundo')
+playlist.insert_beginning('2', 'Baila Baila Baila', 'Ozuna', 'Nibiru')
+playlist.insert_beginning('3', 'Tusa', 'Karol G ft. Nicki Minaj', 'KG0516')
+playlist.insert_beginning('4', 'La Modelo', 'Ozuna ft. Cardi B', 'Singles')
+playlist.insert_beginning('5', 'Ella Quiere Beber', 'Anuel AA ft. Romeo Santos', 'Real Hasta La Muerte')
+
+# Test the playlist methods
+print('Playlist Length:', playlist.playlist_len())
+playlist.play()
+playlist.show_details()
+playlist.next()
+playlist.show_details()
+playlist.previous()
+playlist.show_details()
+playlist.play_shuffle()
+playlist.search_by_name('Tusa')
+playlist.search_by_artist('Ozuna')
+playlist.search_by_artist('Daddy Yankee')
+
