5458d99686abd524c702786e93fdeae0c48fe9b7
Christian <christianbarrios@ufm.edu>
2023-03-03 10:19:58 -0600

    Solution already completed

--- midterm_me/test.py
+++ midterm_me/test.py
@@ -2,6 +2,7 @@ from linked_list import LinkedList, Node
 from stack import Stack
 
 linked_list = LinkedList()
+
 for i in range(10):
     node = Node(str(i))
     linked_list.insert_at_end(node)
@@ -9,19 +10,29 @@ for i in range(10):
 print(linked_list)
 
 def reverse_with_stack(linked_list):
-    stack = Stack()
+    
+    stack = Stack(10)
+    
+    # Put the nodes of the original list on the stack
     for node in linked_list:
         stack.push(node.data)
+        
     reversed_list = LinkedList()
-    while not stack.is_empty():
+    
+    # Create a new list with the nodes of the stack
+    while not stack.top >-1:
         data = stack.pop()
         node = Node(data)
         reversed_list.insert_at_end(node)
+        
     return reversed_list
 
+# Reverse the linked list with a stack
 reversed_list_with_stack = reverse_with_stack(linked_list)
 
+#Reverse the original ll in place
 linked_list.reverse_in_place()
 
+#Compare them
 print(reversed_list_with_stack)
 print(linked_list)
