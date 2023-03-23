d70d304a999b8c0f84c5c7cbac9c31f34c60d40c
Christian <christianbarrios@ufm.edu>
2023-03-23 13:33:08 -0600

    Nuevos archivos

--- activitie_2/main.py
+++ activitie_2/main.py
@@ -3,11 +3,13 @@ import random
 import sys
 from double_linked_list import DoublyLinkedList, Node_song
 from memory_profiler import profile
+from data_persistence import pickle_object, unpickle_object
+
 
 
 @profile
 def insert_songs(ll):
-    for i in range(7000):
+    for i in range(2525):
         ll.insert_beginning('1', 'Dakiti', 'Bad Bunny ft. Jhay Cortez', 'El Último Tour Del Mundo')
         ll.insert_beginning('2', 'Baila Baila Baila', 'Ozuna', 'Nibiru')
         ll.insert_beginning('3', 'Tusa', 'Karol G ft. Nicki Minaj', 'KG0516')
@@ -22,22 +24,15 @@ def traverse(ll):
         pass
 
 
-def save_to_pickle(ll):
-    with open('songs.pkl', 'wb') as f:
-        pickle.dump(ll, f)
-
-
-def load_from_pickle():
-    with open('songs.pkl', 'rb') as f:
-        return pickle.load(f)
-
-
 @profile
 def traverse_with_pickle():
-    ll = load_from_pickle()
-    for i in ll:
+    print(sys.getrecursionlimit())
+    s_2 = unpickle_object('./data_persistence/saved_ll')
+    for node in s_2:
         pass
 
+    # cargar pickle
+    pass
 
 @profile
 def main():
@@ -45,6 +40,9 @@ def main():
     ll = insert_songs(ll)
     save_to_pickle(ll)
     traverse(ll)
+    sys.setrecursionlimit(1000000)
+    print(sys.getrecursionlimit())
+    pickle_object(ll, './data_persistence/saved_ll')
     traverse_with_pickle()
 
 
